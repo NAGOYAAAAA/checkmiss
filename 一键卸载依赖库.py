0e8625fe75d5d727c79f133eef82921d97701942
NAGOYAAAAA <98974867+NAGOYAAAAA@users.noreply.github.com>
2025-05-09 15:10:39 +0800

    Add files via upload

---
+++ 一键卸载依赖库.py
@@ -0,0 +1,19 @@
+import subprocess
+
+# 定义要删除的库列表
+libraries = [
+    'python-dateutil',
+    'pandas',
+    'chardet',
+    'tqdm',
+    'requests'
+]
+
+for library in libraries:
+    try:
+        # 调用 pip uninstall 命令并自动确认删除
+        subprocess.run(['pip', 'uninstall', library, '-y'], check=True)
+        print(f"{library} 库已成功删除。")
+    except subprocess.CalledProcessError as e:
+        print(f"删除 {library} 库时出错: {e}")
+    
