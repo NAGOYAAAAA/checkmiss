70b6f77171be418106b07a56d0659fb097f4a3c5
NAGOYAAAAA <98974867+NAGOYAAAAA@users.noreply.github.com>
2025-05-09 15:17:39 +0800

    快速卸载依赖库

--- 一键卸载依赖库.py
+++ 一键卸载依赖库.py
@@ -5,11 +5,11 @@ libraries = [
     'python-dateutil',
     'pandas',
     'chardet',
-    'tqdm',
+    'tqdm',   “tqdm”,
     'requests'
 ]
 
-for library in libraries:
+for library in   在 libraries:
     try:
         # 调用 pip uninstall 命令并自动确认删除
         subprocess.run(['pip', 'uninstall', library, '-y'], check=True)
